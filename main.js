67e1dae2a614f862af73619431d362ec573f80fe
Goran Ivosevic <goranivosevic@gmail.com>
2017-10-30 12:45:21 +0100

    vue.js

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-document.getElementById("demo").innerHTML ='Today is:' + ' ' + Date();
-function print_page()
-{
-window.print();
-}
-var d = new Date();
-var month = d.getMonth();
-var date = d.getDay();
-var year = d.getFullYear();
-document.getElementById("demo").innerHTML = date + '/ ' + month + '/ ' + year;
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
