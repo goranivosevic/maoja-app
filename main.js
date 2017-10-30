2f666d3137d7e36658d791b53c6e0bf50c09e82c
Goran Ivosevic <goranivosevic@gmail.com>
2017-10-30 11:51:05 +0100

    update

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 document.getElementById("demo").innerHTML ='Today is:' + ' ' + Date();
+function print_page()
+{
+window.print();
+}
+var d = new Date();
+var month = d.getMonth();
+var date = d.getDay();
+var year = d.getFullYear();
+document.getElementById("demo").innerHTML = date + '/ ' + month + '/ ' + year;
